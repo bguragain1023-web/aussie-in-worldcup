4c0cacbc1474d0e37c7a887123c10396e9a46da8
Brazesh Guragain <b.guragain1023@gmail.com>
2026-06-07 21:47:06 +1000

    added generateEmbeddings serves

---
+++ server/services/embeddings.js
@@ -0,0 +1,21 @@
+
+import openAi from "openai"
+
+const openai = new openAI({
+    apiKey: process.env.OPENAI_API_KEY
+})  
+
+export const generateEmbeddings = async(text)=>{
+    try {
+        const response = await openai.embeddings.create({
+            model: "text-embedding-3-small",
+            input: text,
+            encoding_format: "float",
+        })
+        return response.data[0].embedding;
+    } catch (error) {
+        console.error("Error generating embeddings:", error);
+        throw error;
+    }
+
+}
