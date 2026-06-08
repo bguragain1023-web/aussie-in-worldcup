19cf4e1a0e1db9ca79caa79ec236a1c0829801d2
Brazesh Guragain <b.guragain1023@gmail.com>
2026-06-08 18:05:20 +1000

    updated chuncker to chunk based on paragraph

--- server/services/chunker.js
+++ server/services/chunker.js
@@ -1,10 +1,13 @@
-export const chunkText=(text, chunkSize = 500, overlap = 100) => {
+export const chunkText=(text, title) => {
     const chunks = [];
-    let start = 0;
-    while(start < text.length){
-        const end = start + chunkSize;
-        chunks.push(text.slice(start, end));
-        start = end - overlap; // Move back by overlap for the next chunk
-    }
+    const paragraphs = text.split("\n\n").map(p=>p.trim()).filter(p=>p.length > 0);// split paragraph and removee extra spaces and empty paragraphs
+
+  for (let i =0; i< paragraphs.length; i++){
+    const currentParagraph = paragraphs[i];
+    const nextParagraph = paragraphs[i+1] || "";
+    const combinedParagraph = nextParagraph ? `${currentParagraph}\n\n${nextParagraph}` : currentParagraph;
+
+    chunks.push(`${title} \n\n ${combinedParagraph}`);
+  }
     return chunks;
 }
